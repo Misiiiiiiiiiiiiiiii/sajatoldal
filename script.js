a8904f4498c21e5be3ffd8962f304624aef86d3d
Misiiiiiiiiiiiiiiii <59147417+Misiiiiiiiiiiiiiiii@users.noreply.github.com>
2023-10-09 21:48:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,82 @@
-// Form Validation
-const form = document.querySelector('form');
-const nameInput = document.querySelector('#name');
-const emailInput = document.querySelector('#email');
-const messageInput = document.querySelector('#message');
-const error = document.querySelector('.error');
+const darkModeToggle = document.getElementById("darkModeToggle");
+const body = document.body;
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  if (nameInput.value === '' || emailInput.value === '' || messageInput.value === '') {
-    error.textContent = 'Please fill in all fields';
-  } else {
-    error.textContent = '';
-    form.submit();
-  }
+darkModeToggle.addEventListener("click", () => {
+    body.classList.toggle("dark-mode");
+    const moonIcon = darkModeToggle.querySelector(".fa-moon");
+    const sunIcon = darkModeToggle.querySelector(".fa-sun");
+    if (body.classList.contains("dark-mode")) {
+        moonIcon.style.display = "none";
+        sunIcon.style.display = "inline";
+    } else {
+        moonIcon.style.display = "inline";
+        sunIcon.style.display = "none";
+    }
+    saveDarkModePreference();
 });
+
+function loadTasks() {
+    var savedTasks = localStorage.getItem("tasks");
+    if (savedTasks) {
+        var taskList = document.getElementById("taskList");
+        taskList.innerHTML = savedTasks;
+        attachDeleteHandlers();
+    }
+    loadDarkModePreference();
+}
+
+function addTask() {
+    var taskInput = document.getElementById("taskInput");
+    if (taskInput.value !== "") {
+        var taskList = document.getElementById("taskList");
+        var taskItem = document.createElement("li");
+        taskItem.className = "list-group-item";
+        taskItem.innerHTML = taskInput.value + '<button class="btn-delete" onclick="deleteTask(this)"><i class="fas fa-trash"></i></button>';
+        taskList.appendChild(taskItem);
+        taskInput.value = "";
+        attachDeleteHandlers();
+        saveTasks();
+    }
+}
+
+function deleteTask(deleteButton) {
+    var taskItem = deleteButton.parentNode;
+    var taskList = taskItem.parentNode;
+    taskList.removeChild(taskItem);
+    saveTasks();
+}
+
+function saveTasks() {
+    var taskList = document.getElementById("taskList");
+    var tasks = taskList.innerHTML;
+    localStorage.setItem("tasks", tasks);
+}
+
+function attachDeleteHandlers() {
+    var deleteButtons = document.getElementsByClassName("btn-delete");
+    for (var i = 0; i < deleteButtons.length; i++) {
+        deleteButtons[i].addEventListener("click", function () {
+            deleteTask(this);
+        });
+    }
+}
+
+function saveDarkModePreference() {
+    var darkModePreference = body.classList.contains("dark-mode");
+    localStorage.setItem("darkModePreference", darkModePreference);
+}
+
+function loadDarkModePreference() {
+    var darkModePreference = localStorage.getItem("darkModePreference");
+    if (darkModePreference === "true") {
+        body.classList.add("dark-mode");
+        const moonIcon = darkModeToggle.querySelector(".fa-moon");
+        moonIcon.style.display = "none";
+        const sunIcon = darkModeToggle.querySelector(".fa-sun");
+        sunIcon.style.display = "inline";
+    }
+}
+
+loadTasks();
+attachDeleteHandlers();
+loadDarkModePreference();
